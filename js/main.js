b775cc9c95961e36a21fba6a7c8d03f5b6b02c45
Kamil Grabczak <grabczakkamil@gmail.com>
2018-07-06 00:47:39 +0200

    zmiana sekcji about

---
+++ js/main.js
@@ -0,0 +1,20 @@
+    'use strict';
+$(function() {
+    $(".about-question").hide();
+    
+    $(".btn-about-question:nth-of-type(1)").click(function(){
+        $(".about-question:nth-of-type(1)").toggle(1000);
+    })
+    
+    $(".btn-about-question:nth-of-type(2)").click(function(){
+        $(".about-question:nth-of-type(2)").toggle(1000);
+    })
+    
+    $(".btn-about-question:nth-of-type(3)").click(function(){
+        $(".about-question:nth-of-type(3)").toggle(1000);
+    })
+    
+    $(".btn-about-question:nth-of-type(4)").click(function(){
+        $(".about-question:nth-of-type(4)").toggle(1000);
+    })
+});
