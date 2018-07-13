57160aebecc98cb3d91045e8d315b2f8e7b9546d
Kamil Grabczak <grabczakkamil@gmail.com>
2018-07-13 13:39:20 +0200

    dodano slider z lorem ipsum i random img w sekcji portfolio

--- js/main.js
+++ js/main.js
@@ -21,3 +21,55 @@ $(function() {
 
 window.sr = ScrollReveal({reset:true});
 sr.reveal('.scroll-reveall');
+
+/*slider*/
+
+$(function(){
+    'use strict';
+        
+    var slideShow = $('.slide-show');
+    var slideCount = $('.single-slide').length;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
+    
+    slideShow.css('width', 100 * slideCount + '%');
+    
+    slideShow.find('.single-slide').each(function(index) {
+        $(this).css({
+            'width': slideWidth + '%',
+            'margin-left': slideWidth * index + '%'
+        });
+    });
+    $(".prev-slide").click(function(){
+        slide(slideIndex - 1);
+    });
+    
+    $(".next-slide").click(function(){
+        slide(slideIndex + 1);
+    });
+    
+    function slide(newSlideIndex) {
+        if(newSlideIndex >= 0 && newSlideIndex <= (slideCount - 1)) {
+        
+        var slideCaption = $(".slide-caption");
+        var marginLeft = newSlideIndex * -100;
+//        slideCaption.hide();
+        
+        slideShow.animate({"margin-left": marginLeft + '%'}, 800, function(){
+            slideIndex = newSlideIndex;
+//            slideCaption.fadeIn();
+        })
+    }
+else if(newSlideIndex = slideCount) {
+    var slideCaption = $(".slide-caption");
+        var marginLeft = 0;
+//        slideCaption.hide();
+        
+        slideShow.animate({"margin-left": marginLeft + '%'}, 800, function(){
+            slideIndex = 0;
+//            slideCaption.fadeIn();
+        })
+}
+    }
+})
+
