b48174c500e82620e1d80cffc7cf1ee62a37c1ff
Kamil Grabczak <grabczakkamil@gmail.com>
2018-07-18 20:28:42 +0200

    wprowadzono opisy projektow w portfolio, usunieto 2 przyciski social media

--- js/main.js
+++ js/main.js
@@ -71,5 +71,12 @@ else if(newSlideIndex = slideCount) {
         })
 }
     }
-})
+});
 
+$(function () {
+  $("a[href*='#']:not([href='#'])").click(function (e) {
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 600);
+    });
+});
